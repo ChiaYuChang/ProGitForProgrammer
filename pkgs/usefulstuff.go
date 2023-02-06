3c822912b9b91e22cf8f9d151ba1d6cba73eda8e
Chang, Chia Yu <gjerry134679@gmail.com>
2023-02-06 20:56:44 +0800

    add GetUserName, win and linux version

--- pkgs/usefulstuff.go
+++ pkgs/usefulstuff.go
@@ -1,7 +1,30 @@
 package pkgs
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"runtime"
+)
 
 func Greeting(name string) {
 	fmt.Printf("Hello, %s. How are you.\n", name)
+}
+
+func GetUserName() string {
+	switch runtime.GOOS {
+	case "windows":
+		return GetWinUserName()
+	case "linux":
+		return GetLinuxUserName()
+	default:
+		return "unknown os"
+	}
+}
+
+func GetWinUserName() string {
+	return os.Getenv("USERNAME")
+}
+
+func GetLinuxUserName() string {
+	return os.Getenv("USER")
 }
