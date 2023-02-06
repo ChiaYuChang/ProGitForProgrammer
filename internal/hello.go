d51bd5731d46ff33b98f4522688939392c7cc9bb
Chang, Chia Yu <gjerry134679@gmail.com>
2023-02-06 19:46:47 +0800

    add greeting and hellofromwin func

---
+++ internal/hello.go
@@ -0,0 +1,7 @@
+package internal
+
+import "fmt"
+
+func HelloFromWinBranch() {
+	fmt.Println("Hello, I'm from win branch!!")
+}
--- pkgs/usefulstuff.go
+++ pkgs/usefulstuff.go
@@ -1,4 +1,6 @@
-package package
+package pkgs
+
+import "fmt"
 
 func Greeting(name string) {
 	fmt.Printf("Hello, %s. How are you.\n", name)
