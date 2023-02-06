aea0f0fb92dccea199f7ec3e1a57663de5883cd9
cychang <cychang134679@gmail.com>
2023-02-06 19:54:40 +0800

    add GetTime function

--- main.go
+++ main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/ChiaYuChang/ProGitForProgrammer/internal"
 	"github.com/ChiaYuChang/ProGitForProgrammer/pkgs"
 )
 
 func main() {
 	pkgs.Greeting("John")
+	fmt.Println("Current time:", pkgs.GetTime())
 	internal.HelloFromLinuxBranch()
 }
--- pkgs/usefulstuff.go
+++ pkgs/usefulstuff.go
@@ -1,7 +1,14 @@
 package pkgs
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 func Greeting(name string) {
 	fmt.Printf("Hello, %s. How are you.\n", name)
+}
+
+func GetTime() string {
+	return time.Now().Format(time.RFC822)
 }
